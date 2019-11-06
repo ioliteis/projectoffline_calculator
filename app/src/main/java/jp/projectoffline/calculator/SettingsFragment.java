4749b01f117781ad03c49962d52364a5ffeb6478
iolite <iris@eay.jp>
2019-11-06 20:40:50 +0900

    New About screen. Support AndroidX. Bug fixed.

--- app/src/main/java/jp/projectoffline/calculator/SettingsFragment.java
+++ app/src/main/java/jp/projectoffline/calculator/SettingsFragment.java
@@ -1,14 +1,13 @@
 package jp.projectoffline.calculator;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragment {
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //fragment
-        addPreferencesFromResource(R.xml.preference);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preference, rootKey);
     }
 }
